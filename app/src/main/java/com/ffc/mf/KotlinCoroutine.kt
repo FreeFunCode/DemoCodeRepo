b240c6f3a4b6a879460ab3c7968da855d28e6f31
huangguangzhi <940286175@qq.com>
2019-10-19 16:51:19 +0800

    test for hexo new post

--- app/src/main/java/com/ffc/mf/KotlinCoroutine.kt
+++ app/src/main/java/com/ffc/mf/KotlinCoroutine.kt
@@ -2,9 +2,10 @@ package com.ffc.mf
 
 import kotlinx.coroutines.*
 
+@ObsoleteCoroutinesApi
 class KotlinCoroutine {
 
-    fun coroutineDemo1(){
+    fun coroutineDemo1() {
         println("test func coroutineDemo1")
         GlobalScope.launch {
             delay(2000)
@@ -20,7 +21,7 @@ class KotlinCoroutine {
     @ExperimentalCoroutinesApi
     fun testInnerCoroutines() = runBlocking {
         println("[testInnerCoroutines] start ")
-        val request = GlobalScope.launch {
+        val request = GlobalScope.launch() {
             println("主协程 thread:${Thread.currentThread()}")
             val job1 = GlobalScope.launch {
                 println("job1: 独立的协程上下文! thread:${Thread.currentThread()}")
@@ -40,6 +41,51 @@ class KotlinCoroutine {
         request.cancel()
         delay(1000)
         println("main: Who has survived request cancellation?")
+
+        /** ---------------------协程----------------------**/
+
+        /**
+         * 创建
+         */
+        //launch
+        launch{
+                    delay(1000)
+                    println("launch")
+                }
+
+        //async
+        async {
+            delay(1000)
+            println("async")
+            //async 才能有return
+            return@async ""
+        }
+
+
+        /**
+         * 上下文
+         */
+        launch { // 运行在父协程的上下文中，即 runBlocking 主协程
+            println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Default) { // 将会获取默认调度器
+            println("Default               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Main) { // Android主线程
+            println("Main               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.IO) { // 共享后台线程池里的线程
+            println("IO               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Unconfined) { // 不受限的——将工作在主线程中
+            println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(newSingleThreadContext("MyOwnThread")) { // 将使它获得一个新的线程
+            println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        }
+
+
+
     }
 
 }
